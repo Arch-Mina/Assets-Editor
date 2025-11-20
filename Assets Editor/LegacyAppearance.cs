8761df3edb1102fa8946212f6accf4f38a982d08
Zbizu <Zbizu@users.noreply.github.com>
2025-11-20 16:36:18 +0100

    Fix exporting to dat (#75)
    * fix dat export for broken appearances, remove unnecessary includes
    
    * handle situation when flags are null

--- Assets Editor/LegacyAppearance.cs
+++ Assets Editor/LegacyAppearance.cs
@@ -1,14 +1,9 @@
-﻿using Efundies;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tibia.Protobuf.Appearances;
 
 namespace Assets_Editor
